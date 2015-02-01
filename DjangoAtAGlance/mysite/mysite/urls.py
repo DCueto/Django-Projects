87deb3043ef60792e052c10e28d9bebb31357624
DCueto <dcueto14.info@gmail.com>
2015-02-01 19:34:21 +0100

    Finalized DjangoAtAGlance w/ templates

--- DjangoAtAGlance/mysite/mysite/urls.py
+++ DjangoAtAGlance/mysite/mysite/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from news import views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
@@ -8,7 +10,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^articles/(\d{4})/$', views.year_archive),
-    url(r'^articles/(\d{4})/(\d{2})/$', views.month_archive),
-    url(r'^articles/(\d{4})/(\d{2})/(\d+)/$', views.article_detail),
+    #url(r'^articles/(\d{4})/(\d{2})/$', views.month_archive),
+    #url(r'^articles/(\d{4})/(\d{2})/(\d+)/$', views.article_detail),
 
 )
