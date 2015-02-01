887dda8db9f388c6e4ea142fa8e7bcf12dc7dbc2
DCueto <dcueto14.info@gmail.com>
2015-02-01 10:06:46 +0100

    Article URLs

--- DjangoAtAGlance/mysite/mysite/urls.py
+++ DjangoAtAGlance/mysite/mysite/urls.py
@@ -7,4 +7,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^articles/(\d{4})/$', views.year_archive),
+    url(r'^articles/(\d{4})/(\d{2})/$', views.month_archive),
+    url(r'^articles/(\d{4})/(\d{2})/(\d+)/$', views.article_detail),
+
 )
