ea95b87530e199649ca1ca2c660fe9ee4ac8a96f
DCueto <dcueto14.info@gmail.com>
2015-02-17 19:25:52 +0100

    Fixed views.py error. I was using def on class based views

--- WritingFirstDjangoApp/mysite/polls/views.py
+++ WritingFirstDjangoApp/mysite/polls/views.py
@@ -7,7 +7,7 @@ from .models import Question, Choice
 
 # Create your views here.
 
-def IndexView(generic.ListView):
+class IndexView(generic.ListView):
 	template_name = 'polls/index.html'
 	context_object_name = 'latest_question_list'
 
@@ -15,11 +15,11 @@ def IndexView(generic.ListView):
 		""" Return the last five published questions. """
 		return Question.objects.order_by('-pub_date')[:5]
 
-def DetailView(generic.DetailView):
+class DetailView(generic.DetailView):
 	model = Question
 	template_name = 'polls/detail.html'
 
-def ResultsView(generic.DetailView):
+class ResultsView(generic.DetailView):
 	model = Question
 	template_name = 'polls/results.html'
 
