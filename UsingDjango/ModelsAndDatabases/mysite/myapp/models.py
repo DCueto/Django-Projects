c076d3c9cdd562ce00d3134a25fea220092b0352
DCueto <dcueto14.info@gmail.com>
2015-03-19 05:30:07 +0100

    Meta, Model Methods, Overriding predefined model methods

--- UsingDjango/ModelsAndDatabases/mysite/myapp/models.py
+++ UsingDjango/ModelsAndDatabases/mysite/myapp/models.py
@@ -86,5 +86,57 @@ class Membership(models.Model):
 	invite_reason = models.CharField(max_length=64)
 
 
+# Models across files
+
+	# from geography.models import ZipCode
+
+	# class Restaurant(models.Model):
+		# zip_code = models.ForeignKey(ZipCode)
 
 
+# Meta options
+
+class Ox(models.Model):
+	horn_length = models.IntegerField()
+
+	class Meta:
+		verbose_name_plural = "oxen"
+		ordering = ['horn_length']
+
+# Model methods
+
+class CustomPerson(models.Model):
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	birth_date = models.DateField()
+
+	def baby_boomer_status(self):
+		"Returns the person's baby-boomer status."
+		import datetime
+		if self.birth_date < datetime.date(1945,8,1):
+			return "Pre-boomer"
+		elif self.birth_date < datetime.date(1965, 1, 1):
+			return "Baby boomer"
+		else:
+			return "Post-boomer"
+
+	def _get_full_name(self):
+		"Returns the person's full name."
+		return '%s %s' % (self.first_name, self.last_name)
+
+	full_name = property(_get_full_name)
+
+
+# Overriding predefined model methods
+
+class Blog(models.Model):
+	name = models.CharField(max_length=100)
+	tagline = models.TextField()
+
+	def save(self, *args, **kwargs):
+		if self.name == "Yoko Ono's blog":
+			return #Yoko shall never have her own blog!
+		else:
+			super(Blog, self).save(*args, **kwargs) #Call the "real" save() method.
+
+	
