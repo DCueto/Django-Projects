8fa9d1af13738d02bbb90d20924db5029e30a270
DCueto <dcueto14.info@gmail.com>
2015-03-27 18:55:58 +0100

    Init URLDispatcher

---
+++ UsingDjango/HTTPRequests/URLDispatcher/mysite/mysite/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^articles/2003/$', views.special_case_2003),
+    url(r'^articles/(\d{4})/$', views.year_archive),
+    url(r'^articles/(\d{4})/(\d{2})/$', views.month_archive),
+    url(r'^articles/(\d{4})/(\d{2})/(\d+)/$', views.article_detail),
+)
